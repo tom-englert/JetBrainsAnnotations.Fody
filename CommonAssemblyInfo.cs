c8d01b0edeab59f4b41583c09cbaec8e7cf3d58f
Simon Cropp <simon.cropp@gmail.com>
2013-03-23 22:15:32 +1100

    move over to fody nuget

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Virtuosity")]
 [assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
