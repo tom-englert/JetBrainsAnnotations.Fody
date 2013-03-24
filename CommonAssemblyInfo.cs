974b538942e696809ffcb9a2ed658a2758bbec1c
Simon Cropp <simon.cropp@gmail.com>
2013-03-24 17:29:12 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("Virtuosity")]
-[assembly: AssemblyProduct("Virtuosity")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyTitle("JetBrainsAnnotations")]
+[assembly: AssemblyProduct("JetBrainsAnnotations")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
