bdac6075edd0be22ba51e10a6e8f71183f8f83a7
Simon Cropp <simon.cropp@gmail.com>
2013-07-09 22:25:38 +1000

    fix VS Guid

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("JetBrainsAnnotations")]
 [assembly: AssemblyProduct("JetBrainsAnnotations")]
-[assembly: AssemblyVersion("0.2.2.0")]
-[assembly: AssemblyFileVersion("0.2.2.0")]
+[assembly: AssemblyVersion("0.2.3")]
+[assembly: AssemblyFileVersion("0.2.3")]
